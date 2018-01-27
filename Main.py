c66cf4bb522cb2e57993f27c7d88147983b761a4
guy.niv <guy.niv@mail.huji.ac.il>
2018-01-27 19:15:39 +0200

    some improvements

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ import numpy as np
 T = 'T'
 M = 'M'
 TAKEN = 'X'
-MAX_R = 5
-MAX_C = 5
+MAX_R = 10
+MAX_C = 10
 
 class Slice:
 
@@ -14,7 +14,7 @@ class Slice:
         self.r2 = r2
         self.c2 = c2
 
-    def cells(self):
+    def get_cells(self):
         return (self.r2 - self.r1 + 1) * (self.c2 - self.c1 + 1)
 
     def __str__(self):
@@ -25,12 +25,12 @@ def parse(input):
     return [line.rstrip('\n') for line in open(input)]
 
 
-def is_legal(arr, L, H):
+def is_legal(arr, L):
     if TAKEN in arr:
         return False
     T_counter = len(np.where(arr == T)[0])
-    M_counter = len(np.where(arr == M)[0])
-    return T_counter <= H and T_counter >= L and M_counter <= H and M_counter >= L
+    M_counter = len(arr) - T_counter
+    return T_counter >= L and M_counter >= L
 
 
 if __name__ == "__main__":
@@ -49,20 +49,19 @@ if __name__ == "__main__":
     indexes = np.zeros((R, C), dtype=np.bool)
     r, c = 0, 0
     indexes[0][0] = True
+    dir_x, dir_y = 1, 1
+    success = False
     while True:
+        maxi = -1, -1
         helper = np.zeros((MAX_R, MAX_C))
         for i in range(MAX_R):
             for j in range(MAX_C):
-                if r + i < R and c + j < C and is_legal(pizza[r:r + i + 1, c:c + j + 1], L, H):
-                    helper[i][j] = (i + 1) * (j + 1)
-        if not np.all(helper == 0):
-            maxi = 0, 0
-            maxe = 0
-            for i in range(MAX_R):
-                for j in range(MAX_C):
-                    if helper[i][j] > maxe:
+                size = (i + 1) * (j + 1)
+                if r + i < R and c + j < C and is_legal(pizza[r:r + i + 1, c:c + j + 1], L) and size <= H:
+                    helper[i][j] = size
+                    if size > (maxi[0 + 1]) * (maxi[1] + 1):
                         maxi = i, j
-                        maxe = helper[i][j]
+        if maxi != (-1, -1):
             slices.append(Slice(r, c, r + maxi[0], c + maxi[1]))
             indexes[r:r + maxi[0] + 1, c:c + maxi[1] + 1] = True
             pizza[r:r + maxi[0] + 1, c:c + maxi[1] + 1] = TAKEN
@@ -70,7 +69,13 @@ if __name__ == "__main__":
         indexes[r][c] = True
         if False not in indexes:
             break
-        # next r and c:
+
+        # better option for choosing r and c:
+
+
+
+
+        # choose next r and c:
         left = np.where(indexes == False)
         rand = np.random.choice(len(left[0]), 1)[0]
         r = left[0][rand]
@@ -79,7 +84,8 @@ if __name__ == "__main__":
     summation = 0
     print(len(slices))
     for slice in slices:
-        summation += slice.cells()
+        summation += slice.get_cells()
         print(slice)
-    print(pizza)
-    print(summation)
+
+    print("\nPizza:\n", pizza)
+    print("Number of cells: " + str(summation))
